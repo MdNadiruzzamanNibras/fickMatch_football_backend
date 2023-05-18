d9082115da5b05aacb6ed0b80f241965f6dfa801
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2023-05-18 21:36:05 +0600

    get method use

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = 5000
 
+// middleware
+app.use(cors())
+app.use(express.json())
 
-const uri = `mongodb+srv://xff:2e8oQw0PsLup1Y90@cluster0.5rgf1kx.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5rgf1kx.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -16,11 +22,11 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+      client.connect();
     // Send a ping to confirm a successful connection
     const footballCollection = client.db('footballCollection').collection('matchs');
     console.log("You successfully connected to MongoDB!");
-    app.get('/matchs', async(req,res)=>{
+    app.get('/getMatchStats', async(req,res)=>{
       const qurey = {}
       const cursor =  footballCollection.find(qurey)
       const matchs = await cursor.toArray()
